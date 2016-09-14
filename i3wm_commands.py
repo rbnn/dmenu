a0bcc335b98da3562a44d3637924a0ccbd2149f8
Ronny Biemann <rbnn@users.noreply.github.com>
2016-09-14 19:10:59 +0200

    Remove i3wm_workspace_commands
    File i3wm_workspace_commands.py has been removed. All logic has been
    transferred into file i3wm_commands.py.

--- i3wm_commands.py
+++ i3wm_commands.py
@@ -6,7 +6,17 @@ def i3wm_safe_name(name):
   if ' ' not in name: return name
   else: return '"%s"' % name
 
-execfile('i3wm_workspace_commands.py')
+def i3wm_workspace_commands(i3, out = sys.stdout):
+  ws_names  = [i3wm_safe_name(x['name']) for x in i3.get_workspaces()]
+  out_names = [i3wm_safe_name(x['name']) for x in i3.get_outputs()]
+  out_default = ['left', 'right', 'down', 'up']
+
+  # Rename workspaces
+  out.writelines(['rename workspace %s to \n' % x for x in ws_names])
+
+  # Move workspaces
+  out.writelines(['move workspace to output %s\n' % x for x in out_names])
+  out.writelines(['move workspace to output %s\n' % x for x in out_default])
 
 i3 = i3ipc.Connection()
-i3wm_workspace_commands(i3)
+i3wm_workspace_commands(i3, sys.stdout)
