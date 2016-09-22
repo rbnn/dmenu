538ed7f37b5ebed9f9b30a81515ada97c903e60b
Ronny Biemann <rbnn@users.noreply.github.com>
2016-09-22 11:21:37 +0200

    Add (un)install targets to Makefile

--- Makefile
+++ Makefile
@@ -1,13 +1,11 @@
 # dmenu - dynamic menu
 # See LICENSE file for copyright and license details.
+.PHONY: dmenu-{debug,release} install uninstall
 
 include config.mk
 
 CFLAGS += `pkg-config --cflags ${PACKAGES}`
 LDFLAGS += `pkg-config --libs ${PACKAGES}`
-
-RELEASE_CFLAGS = -g0 -O2 -DNDEBUG
-DEBUG_CFLAGS = -g3 -O0
 
 dmenu-release: CFLAGS += ${RELEASE_CFLAGS}
 dmenu-release: dmenu
@@ -17,3 +15,11 @@ dmenu-debug: dmenu
 
 dmenu: controller.o dmenu.o inputbuffer.o util.o viewer.o x.o xcmd.o
 	$(CC) -o $@ ${LDFLAGS} $?
+
+install: dmenu-release
+	@$(INSTALL) --strip --mode=755 {,${PREFIX}/bin/}dmenu
+	@$(INSTALL) --mode=755 {,${PREFIX}/bin/}dmenu_run
+	@$(INSTALL) --mode=755 {,${PREFIX}/bin/}dmenu-i3_run
+
+uninstall:
+	@$(RM) ${PREFIX}/bin/{dmenu,dmenu_run,dmenu-i3_run}
--- config.mk
+++ config.mk
@@ -1,8 +1,12 @@
 # dmenu version
 VERSION = 4.6x
 
-# paths
-PREFIX = /usr/local
+# Install program (-D Create paths recursively)
+INSTALL=install -D --verbose --preserve-timestamps
+RM=rm -fv
+
+# Paths (they're DESTDIR sensible)
+PREFIX = ${DESTDIR}/usr
 MANPREFIX = ${PREFIX}/share/man
 
 # Select packages
@@ -12,12 +16,14 @@ PACKAGES = glib-2.0 fontconfig libconfig x11 xft
 PACKAGES += xinerama
 CFLAGS += -DXINERAMA
 
-# flags
+# Flags
 # CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700 -D_POSIX_C_SOURCE=200809L -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
 CPPFLAGS = -DVERSION=\"${VERSION}\"
 # CFLAGS   = -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
 CFLAGS += -Wall ${CPPFLAGS}
-# LDFLAGS  = -s ${LIBS}
 
-# compiler and linker
+RELEASE_CFLAGS = -g0 -O2 -DNDEBUG
+DEBUG_CFLAGS = -g3 -O0
+
+# Compiler and linker
 CC = cc
